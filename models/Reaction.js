fd93d115f5869518ad387e5ea553500d69bea09a
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-06 22:54:14 -0700

    Created reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,39 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+
+);
+
+formatDate = (createdAt) => {
+    const date = createdAt.toString();
+    return moment(date).format("MMM Do, YYYY")
+} 
+
+module.exports = reactionSchema;
+
