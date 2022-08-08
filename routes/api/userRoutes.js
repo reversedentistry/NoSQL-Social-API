c10f35144652faf0a4d1ef3c0c70793a9de88290
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-07 21:13:03 -0700

    Created user api routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    //editUser
+    deleteUser
+} = require("../../controllers/userController");
+
+router.route("/").get(getUsers).post(createUser); 
+
+router.
+route("/:userId")
+.get(getSingleUser)
+//.put(editUser)
+.delete(deleteUser); 
+
+module.exports = router; 
