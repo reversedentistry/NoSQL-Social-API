cbb0ead7d633bb6da1aacc7641e324d89715eeaf
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-08 20:48:38 -0700

    Created post and delete commands

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -16,8 +16,37 @@ module.exports = {
                     : res.json({ thought })
             )
             .catch((err) => {
-                console.log(err); 
+                console.log(err);
                 return res.status(500).json(err);
             });
-    }, 
+    },
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
+    },
+    //editThought
+    deleteThought(req, res) {
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "No such thought exists." })
+                    : User.findOneAndUpdate(
+                        { thoughts: req.params.thoughtId },
+                        { $pull: { thoughts: req.params.thoughtId } },
+                        { new: true }
+                    )
+            )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({
+                        message: "Thought deleted, but no user found."
+                    })
+                    : res.json({ message: "Thought successfully deleted." })
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
 }
