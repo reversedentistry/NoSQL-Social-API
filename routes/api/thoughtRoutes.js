f9ff77f87b674a0ce0bb7c24a06b2ebaf381d341
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-10 21:34:32 -0700

    Created POST route for thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,12 @@
 const router = require("express").Router(); 
+const {
+    getThoughts, 
+    getSingleThought, 
+    createThought, 
+    editThought, 
+    deleteThought,
+} = require("../../controllers/thoughtController"); 
+
+router.route('/').get(getThoughts).post(createThought);
 
 module.exports = router; 
