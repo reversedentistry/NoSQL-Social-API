074672fed27e7d3f6fa4e61c1b3625bb5c757ebb
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-10 22:08:10 -0700

    Created reaction controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -73,4 +73,30 @@ module.exports = {
                 res.status(500).json(err);
             });
     },
+    addReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "No thought with this ID" })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    }, 
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId }, 
+            { $pull: { reactions: { reactionId: req.params.reactionId } } }, 
+            { runValidators: true, new: true }
+        )
+        .then((thought) =>
+        !thought
+                    ? res.status(404).json({ message: "No thought with this ID" })
+                    : res.json(thought)
+        ) 
+        .catch((err) => res.status(500).json(err));
+    }
 }
