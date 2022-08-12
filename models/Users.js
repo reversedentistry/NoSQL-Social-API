ad68be7327f0bcd0504a2661d026c8d0e251bbcd
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-11 23:31:11 -0700

    Added email validation

--- models/Users.js
+++ models/Users.js
@@ -12,7 +12,7 @@ const userSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            // some kind of email match validation 
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please use a valid email address'] 
         },
         thoughts: [
             {
